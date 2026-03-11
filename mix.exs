5110c1e21514a7ed8fe4715b550729e54b95a856
saatsky <sergio.abreu@skyline.be>
2026-03-11 14:59:16 +0000

    bump version 0.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HierarchyPai.MixProject do
   def project do
     [
       app: :hierarchy_pai,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
